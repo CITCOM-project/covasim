9a0c0023f3f7bd87eb3c133eb1f21e9f022a132e
AndrewC19 <agclark2@sheffield.ac.uk>
2021-06-22 13:19:43 +0100

    Added helper method

--- causcumber/scenarios/compare_interventions/compare_interventions_sim.py
+++ causcumber/scenarios/compare_interventions/compare_interventions_sim.py
@@ -1,5 +1,12 @@
 import covasim as cv
-from causcumber.helper_functions import msim_to_csv
+
+
+def msim_to_csv(simulation: cv.MultiSim, output_file_name: str):
+    """ Takes a MultiSim simulation, converts to a CSV file, and saves to ./results/ with the
+        given file name. """
+    simulation_df = simulation.base_sim.to_df()
+    simulation_df.to_csv("./results/{}.csv".format(str.lower(output_file_name)), index=False)
+
 
 """ Which intervention is more effective at reducing the cumulative number of infections? """
 base_pars = dict(
